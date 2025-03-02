74e0cd9c992c5c5285aba37c66fb55c0aaadef00
Supun Pramod <pramodsupun06@gmail.com>
2025-03-02 12:03:40 +0530

    we put what we need in the tailwind.config.js😒👌

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,30 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        poppins:["Poppins","sans-serif"],
+      },
+      colors:{
+        primary:"#f7ba34",
+        secondary:"#69a79c",
+        light:"f7f7f7",
+        dark:"#333333",
+        dark2:"#999999",
+      },
+      container:{
+        center:true,
+        padding:{
+          DEFAULT:"1rem",
+          sm:"2rem",
+          lg:"4rem",
+          xl:"5rem",
+          "2xl":"6rem",
+        },
+
+      },
+
+    },
   },
   plugins: [],
 }
