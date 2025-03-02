5ccf8786ccb07ef26228509e21df95fe397518cf
Supun Pramod <pramodsupun06@gmail.com>
2025-03-02 11:49:16 +0530

    delete the app.css and modify the app.jsx in our project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     ffgg fggg
-    </>
+const App=()=>{
+  return(
+    <div className='bg-red-500'>App</div>
   )
-}
+};
 
 export default App
