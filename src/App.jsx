c258339e0091487d5cd8d54870bef7ffb15c3cdb
Supun Pramod <pramodsupun06@gmail.com>
2025-03-03 16:08:04 +0530

    create the banner.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import Services from './components/Services/Services';
+import Banner from './components/Banner/Banner';
 
 
 
@@ -10,7 +11,8 @@ const App=()=>{
     <main className="overflow-x-hidden bg-white text-dark">
      
      <Hero/>
-     <Services/>
+     <Services/> 
+     <Banner/>
       </main>
   );
 };
